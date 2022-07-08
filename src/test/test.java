684b0524a15fdceb045208c9f1c2e38336724077
Snow <thingoctuyettruong@gmail.com>
2022-07-08 20:19:52 +0700

    Thêm các dòng sysout nhé

--- src/test/test.java
+++ src/test/test.java
@@ -3,5 +3,8 @@ package test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
+		System.out.println("Hello World 2 !!!!");
+		System.out.println("Hello World 3 !!!!");
+		System.out.println("Hello World 4 !!!!");
 	}
 }
