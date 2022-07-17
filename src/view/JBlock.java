4f8b991ad7257d24ec02bc7c5c1cdb81a3d10d24
Snow <thingoctuyettruong@gmail.com>
2022-07-17 22:21:25 +0700

    đổi background

--- src/view/JBlock.java
+++ src/view/JBlock.java
@@ -34,20 +34,52 @@ public class JBlock extends JButton{
 		case "cell_true":
 			g.drawImage(imgs.getList_imgs().get("red_cell"), 0, 0, getPreferredSize().width, getPreferredSize().height, null);
 			if(number<10) {
-				g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number)),  40, 40, 40, 40, null);				
+				if(level ==1)
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number)),  40, 40, 40, 40, null);				
+				if(level ==2)
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number)),  28, 28, 28, 28, null);				
+				else 
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number)), 20, 20, 20, 20, null);				
 			}else {
-				g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number/10)),  20, 40, 40, 40, null);				
-				g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number%10)),  60, 40, 40, 40, null);	
+				if(level==1) {
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number/10)),  20, 40, 40, 40, null);				
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number%10)),  60, 40, 40, 40, null);		
+				}
+				if(level==2) {
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number/10)),  15, 26, 26, 26, null);				
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number%10)),  40, 26, 26, 26, null);	
+					System.out.println("Đây là level 2");
+				}
+				else {
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number/10)), 10, 20, 20, 20,null);		
+					System.out.println("Đây là level 3 ");
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number%10)), 30, 20, 20, 20, null);		
+				}
 			}
 			
 			break;
 		case "cell_false":
 			g.drawImage(imgs.getList_imgs().get("blue_cell"), 0, 0, getPreferredSize().width, getPreferredSize().height, null);
 			if(number<10) {
-				g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number)),  40, 40, 40, 40, null);				
+				if(level ==1)
+					g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number)),  40, 40, 40, 40, null);	
+				if(level==2)
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number)),  27, 27, 27, 27, null);		
+				else 
+					g.drawImage(imgs.getList_imgs().get("r"+String.valueOf(number)),  20, 20, 20, 20, null);	
 			}else {
-				g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number/10)),  20, 40, 40, 40, null);				
-				g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number%10)),  60, 40, 40, 40, null);	
+				if(level ==1) {					
+					g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number/10)),  20, 40, 40, 40, null);				
+					g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number%10)),  60, 40, 40, 40, null);	
+				}
+				if(level ==2) {					
+					g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number/10)),  15, 26, 26, 26, null);				
+					g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number%10)),  40, 26, 26, 26, null);	
+				}
+				else {					
+					g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number/10)), 10, 20, 20, 20,null);				
+					g.drawImage(imgs.getList_imgs().get("b"+String.valueOf(number%10)), 30, 20, 20, 20, null);	
+				}
 			}
 			break;
 		case "cell_null":
